a749fe1e099b56162226be0acf84376819212473
Katrina Fuller <trinafuller@att.net>
2019-08-13 16:54:40 -0600

    worked on conditionals

--- app/conditionals.js
+++ app/conditionals.js
@@ -14,7 +14,9 @@ function largerNum(num1, num2) {
 // output: "F"
 
 function testGrader(score, possible) {
-
+    if (score == possible) {
+        return "A";
+    }
 }
 
 
@@ -30,12 +32,27 @@ function testGrader(score, possible) {
 
 function timeOfDayGreeting(hour) {
     // you code below
+    if (hour >= 5 && hour <= 11) {
+        return "good morning";
+    } else if (hour >= 12 && hour <= 17) {
+        return "good afternoon";
+    } else if (hour >= 18 && hour <= 21) {
+        return "good evening";
+    } else {
+        return "good night";
+    }
 
 }
 
 //4. Write a function that will take in a number and return 'fever' if it indicates a fever (over 98.6) and additionally if the person should go to the hospital (at or above 103) 'fever go to hospital' (hint: try this with string concatenation), if it is under return 'no fever'
 function isFever(temp) {
-
+    if (temp >= 103) {
+        return "fever go to hospital";
+    } else if (temp > 98.6) {
+        return "fever";
+    } else {
+        return "no fever";
+    }
 }
 
 //5. Write a function that takes in a car object, if it is not moving then return true
@@ -47,6 +64,11 @@ let myCar = {
 }
 
 function isStopped(car) {
+    if (car.moving == false) {
+        return true;
+    } else {
+        return false;
+    }
 
 }
 
@@ -58,5 +80,9 @@ let dish = {
 }
 
 function washDish(dish) {
-
+    if (dish.yourDish == true && dish.isDirty == true) {
+        return true;
+    } else {
+        return false;
+    }
 }
