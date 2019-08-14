115d94ccb46e81ef3b16689bfb81b8efac82ee52
Katrina Fuller <trinafuller@att.net>
2019-08-13 21:50:18 -0600

    finished bonus section

--- app/bonuses.js
+++ app/bonuses.js
@@ -2,5 +2,22 @@
 let str = "team number 2"
 
 function camelCase(str) {
+  let camelCasedStr = "";
+  let capNext = false;
+  for (let i = 0; i < str.length; i++) {
 
+    if (str[i] == " ") {
+      capNext = true;
+    } else {
+      if (capNext) {
+        camelCasedStr += str[i].toUpperCase()
+        capNext = false;
+      } else {
+        camelCasedStr += str[i]
+      }
+
+    }
+  }
+  camelCasedStr[0] = camelCasedStr[0].toLowerCase()
+  return camelCasedStr;
 }
